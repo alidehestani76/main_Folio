eb8b328949be2d18c80ab2880e8ae0639d82fe4a
alidehestani76 <alidehestani76@gmail.com>
2018-04-20 13:08:12 +0430

    Api

--- Backend/Bourse/Bourse/settings.py
+++ Backend/Bourse/Bourse/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Bourseapp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- Backend/Bourse/Bourse/urls.py
+++ Backend/Bourse/Bourse/urls.py
@@ -20,4 +20,5 @@ from django.conf.urls import url , include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^Bourse/', include('Bourseapp.urls')),
+    url(r'^api/',include('Bourseapp.api.urls')) ,
 ]
