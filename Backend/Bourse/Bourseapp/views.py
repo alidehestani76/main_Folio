b271c5e26636980f856c5d6ec116f3fd6042e4c4
aSafarpoor <alisafarpoor1995@gmail.com>
2018-04-27 15:10:20 +0430

    info api

--- Backend/Bourse/Bourseapp/views.py
+++ Backend/Bourse/Bourseapp/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from Bourseapp.models import Bourse
 from Bourseapp.cron import From_Bourse
+from Personapp.models import Person
 
 # def Bourse(request) :
 #     b=Bourse()
@@ -12,10 +13,22 @@ from Bourseapp.cron import From_Bourse
 
 #faghat yadam bemoone:D
 def Bourse(request):
-    if 'q' in request.GET:
-        message = 'You searched for: %r' % request.GET['q']
+#     if 'q' in request.GET:
+#         message = 'You searched for: %r' % request.GET['q']
+#     else:
+#         message = 'You submitted an empty form.'
+#     file1 = open("/home/wt/Desktop/hasan.txt", "a")
+#     file1.write(request.GET['q'])
+
+    my_q=request.GET['q'].split('*')
+    is_exist = Person.objects.filter(user_name=my_q[0]).exists()
+    if(is_exist==True):message='repeated username'
     else:
-        message = 'You submitted an empty form.'
-    file1 = open("/home/wt/Desktop/hasan.txt", "a")
-    file1.write(request.GET['q'])
+        message='created'
+        person=Person()
+        person.user_name=my_q[0]
+        person.name = my_q[1]
+        person.lastname =my_q[2]
+        person.save()
+
     return HttpResponse(message)
