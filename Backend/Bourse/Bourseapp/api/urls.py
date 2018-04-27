aee2f454a87bcde14563e1c4b21fe9cd3f0f1d91
aSafarpoor <alisafarpoor1995@gmail.com>
2018-04-27 11:02:10 +0430

    info api

--- Backend/Bourse/Bourseapp/api/urls.py
+++ Backend/Bourse/Bourseapp/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
 
-    url(r'^api',views.PostListAPIview.as_view()) ,
+    url(r'^all_information',views.PostListAPIview.as_view()) ,
     # url(r'^(?P<id>[0-9]{1,3})',views.PostDetailsAPIview.as_view()),
     #url(r'^(?P<namad>)',views.PostDetailsAPIview.as_view()),
     #url(r'^(?P<namad>\w+)$', views.PostDetailsAPIview.as_view()),        #.as_view() to turn Class into Vie
