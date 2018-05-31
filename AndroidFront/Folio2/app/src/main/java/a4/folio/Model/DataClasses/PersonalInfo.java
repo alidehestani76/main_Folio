8785c767b678a0f62a6b79da4fac119947b0966f
alidehestani76 <alidehestani76@gmail.com>
2018-05-31 16:39:16 +0430

    fix push

--- AndroidFront/Folio2/app/src/main/java/a4/folio/Model/DataClasses/PersonalInfo.java
+++
@@ -1,8 +0,0 @@
-package a4.folio.Model.DataClasses;
-
-/**
- * Created by amir on 4/18/2018.
- */
-
-public final class PersonalInfo {
-}
--- AndroidFront/Folio2/app/src/main/java/a4/folio/View/ReportPageView.java
+++
@@ -1,8 +0,0 @@
-package a4.folio.View;
-
-/**
- * Created by amir on 4/20/2018.
- */
-
-public class ReportPageView {
-}
